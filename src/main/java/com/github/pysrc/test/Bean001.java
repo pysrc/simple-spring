cc24d8241cf2aa1b51b150fb086f5cf7b9a8f3f4
pysrc <1570184051@qq.com>
2019-08-16 20:53:36 +0800

    正式一点

--- src/main/java/com/github/pysrc/test/Bean001.java
+++ src/main/java/com/github/pysrc/test/Bean001.java
@@ -4,7 +4,7 @@ import com.github.pysrc.ann.Component;
 
 @Component
 public class Bean001 {
-    String name = "Bean001";
+    String name;
 
     public String getName() {
         return name;
--- src/main/java/com/github/pysrc/test/Main.java
+++ src/main/java/com/github/pysrc/test/Main.java
@@ -8,7 +8,18 @@ import com.github.pysrc.context.ApplicationContext;
 public class Main {
     public static void main(String[] args) throws Exception {
         Context context = ApplicationContext.run(Main.class, args);
+        // 从容器中取出bean003
         Bean003 bean003 = context.getBean(Bean003.class);
+
+
+        // 此时bean001的name还为空
+        System.out.println("------------------Bean001'name is null------------------------");
+        System.out.println(bean003.getBean002().getBean001().getName());
+
+        // 将容器中bean001的name字段设为非空
+        Bean001 bean001 = context.getBean(Bean001.class);
+        bean001.setName("This is bean001 !");
+        System.out.println("------------------Bean001'name is not null------------------------");
         System.out.println(bean003.getBean002().getBean001().getName());
     }
 }
